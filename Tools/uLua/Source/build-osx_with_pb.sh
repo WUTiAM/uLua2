7431f1df6147d4da24e168c4258e060cc9547a8e
islet8.JO3L <islet8@gmail.com>
2016-06-01 16:04:25 +0800

    Update building ulua.bundle for Mac OSX.

--- Tools/uLua/Source/build-osx_with_pb.sh
+++ Tools/uLua/Source/build-osx_with_pb.sh
@@ -5,10 +5,12 @@ LUAJIT_VER="lua-5.1.5"
 # Build liblua.a for x86_64
 make -C $LUAJIT_VER clean
 make -j2 -C $LUAJIT_VER BUILDMODE=static
+# Build ulua.bundle
 cp $LUAJIT_VER/src/liblua.a osx/liblua-x86_64.a
-
 cd osx/
 xcodebuild
 cd ..
-
+# Copy to target folder
 cp -r osx/Build/Release/ulua.bundle Plugins/
+
+make -C $LUAJIT_VER clean
